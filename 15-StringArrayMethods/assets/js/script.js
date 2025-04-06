4e585b6edab1e6827126bda699b67e79c7d9cabf
nargiiziii <ibrahimlinargiz169@gmail.com>
2025-04-06 00:46:55 -0800

    15th task in progress

--- 15-StringArrayMethods/assets/js/script.js
+++ 15-StringArrayMethods/assets/js/script.js
@@ -7,23 +7,27 @@
 // ################ 1.Verilmis string-de sait herflewrin tapilmasi ################
 
 
+// let str = "I am frontend DEVELOPER I LEARN Javascript";
+// let vowels = "aeiouAEIOU";  //saitleri ozum yazib verirem ki metod ile icinnen tapim
 
-let str = "I am frontend DEVELOPER I LEARN Javascript";
+// function findVowels(str) {
+//     // debugger
+//     let result =[];  //bos bir array yaratdim ki icinde saitleri yigim
+//     for (let i = 0; i < str.length; i++) {
+//         if (str.includes(vowels[i])) {   //stringin icinde sait varsa bu if blokuna girir 
+//             result.push(vowels[i]);   //sait varsa o saiti siraynan goturub result arrayine elave edir
+//         }
+//     }
+//     return result;
+// }
+// console.log(findVowels(str)); // ['a', 'e', 'i', 'o', 'A', 'E', 'I', 'O'] burda verilen siralama menim vowels string'imde olan saitlerin sirasina goredi
 
-let vowels = "aeiouAEIOU";  //saitleri ozum yazib verirem ki metod ile icinnen tapim
 
-function findVowels(str) {
-    debugger
-    let result =[];  //bos bir array yaratdim ki icinde saitleri yigim
-    for (let i = 0; i < str.length; i++) {
-        if (str.includes(vowels[i])) {   //stringin icinde sait varsa bu if blokuna girir 
-            result.push(vowels[i]);   //sait varsa o saiti siraynan goturub result arrayine elave edir
-        }
-    }
-    return result;
-}
 
-console.log(findVowels(str)); // ['a', 'e', 'i', 'o', 'A', 'E', 'I', 'O'] burda verilen siralama menim vowels stringimde olan saitlerin sirasina goredi
+
+
+
+
 
 
 
@@ -33,8 +37,53 @@ console.log(findVowels(str)); // ['a', 'e', 'i', 'o', 'A', 'E', 'I', 'O'] burda 
 // ################ 2.Verilmish string-de sozlerin bosluga gore sayi. ################
 
 
+// let str = "I am frontend DEVELOPER I LEARN Javascript";
+
+// function wordsCount() {
+//     let words = str.split(" ");  //stringi bosluqlara gore ayirdim ve arraye cevirdim
+//     words = words.length;
+//     return words;
+// }
+// console.log(wordsCount(str));
+
+
+
+
+
+
+
+
+
+
+
 
 // ################ 3.Verilmiş stringin-in ən uzun sözünü ekrana çıxaran proqram yazın ################
+
+
+// let str = "I am frontend DEVELOPER I LEARN Javascript";
+
+// function longestWord() {
+//     let words = str.split(" "); //cumleni sozlere ayirdim 
+
+//     let longest = words.reduce((longest, word) => {  // reduce metodunu istifade etdim, ve birinci tapmaq istediyim sozu yazdim icine(longest) sonrada hazirda olan soz(word)
+//         // debugger
+//         if (word.length > longest.length) { 
+//             longest = word; 
+//         }
+//         return longest; 
+//     })
+//     return longest;
+// }
+// console.log(longestWord(str)); 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,7 +91,46 @@ console.log(findVowels(str)); // ['a', 'e', 'i', 'o', 'A', 'E', 'I', 'O'] burda 
 
 
 
+// let str = "I am frontend DEVELOPER I LEARN Javascript";
+// let words = str.split(" "); //cumleni sozlere ayirdim
+
+// function findWord() {
+//     let newArr = [];  //yeni array yaratdim cunki forEach metodu ozu ozune yaratmir yeni massiv. newArr'in icine yigacam sozleri ve indexlerini
+//     words.forEach((word, index) => {
+//         // debugger
+//         if (word === word.toUpperCase()) {
+//             return newArr.unshift( {word,index});  //push ilede elave ede bilerdim, amma ferqli olsun deye unshift isletdim
+//         }
+//     })
+//     return newArr;
+// }
+// console.log(findWord(words));
+
+
+
+
+
+
+
+
 // ################ 5.Verilmiş string-in 2-dən artıq böyük hərfi olan elementlərini çapa çıxaran proqram yazın ################
+
+
+
+
+
+let str = "I am frontend DEVELOPER I LEARN Javascript";
+
+
+
+function findUpperCase() {
+    let words = str.split(" ");
+    
+
+}
+
+
+
 
 
 
