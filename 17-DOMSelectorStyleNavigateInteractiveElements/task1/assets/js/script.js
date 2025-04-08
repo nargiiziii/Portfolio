754ef8e0373dcb47eb209831ed3c10bc1f649761
nargiiziii <ibrahimlinargiz169@gmail.com>
2025-04-08 04:48:07 -0800

    17th task in procces(completed first task)

---
+++ 17-DOMSelectorStyleNavigateInteractiveElements/task1/assets/js/script.js
@@ -0,0 +1,92 @@
+
+//##### 1) 
+
+// birinci usul QUERYSELECTOR ile
+
+
+
+// let heart = document.querySelector(".heart");
+// let card = heart.parentElement.parentElement;
+
+// let image = card.querySelector("img").src;
+// let cardTitle = card.querySelector(".cardTitle").textContent;
+// let cardText = card.querySelector(".cardText").textContent;
+// let cardPrice = card.querySelector(".cardPrice").textContent;
+
+// let cardInfo = {
+//     image: image,
+//     title: cardTitle,
+//     text: cardText,
+//     price: cardPrice
+// }
+// console.log(cardInfo);
+
+
+
+
+
+
+
+// ikinci usul CLOSEST islederek
+
+
+
+// let heart = document.querySelector(".heart");
+// let card = heart.closest(".card");
+
+// let image = card.querySelector("img").src;
+// let title = card.querySelector(".cardTitle").textContent;
+// let text = card.querySelector(".cardText").textContent;
+// let price = card.querySelector(".cardPrice").textContent;
+
+// let cardInfo = {
+//     image,
+//     title,
+//     text,
+//     price 
+// }
+// console.log(cardInfo);
+
+
+
+
+
+
+
+
+
+//##### 2) 
+
+
+
+
+
+
+
+//##### 3)
+
+
+
+// let card = document.querySelector(".card");
+
+// let img = card.querySelector("img");
+// img.src = "https://avatars.mds.yandex.net/i?id=68495fdccbf5a3b5fb8bcce557ab0a351eb91ad2-5713119-images-thumbs&n=13";
+// console.log(img);
+
+
+
+
+// let cardContent = card.querySelector(".cardContent"); //card ile elementleri goturmeye davam ede bilerdim, amma daha deqiq olsun deye cardContente girdim, onun icinnen cixardim title,text,price
+
+// let title = cardContent.querySelector(".cardTitle");
+// title.textContent = "house";
+// console.log(title);
+
+// let text = cardContent.querySelector(".cardText");
+// text.textContent = "very big and comfortable house, buy it!";
+// console.log(text);
+
+// let price = cardContent.querySelector(".cardPrice");
+// price.textContent = "100.000$";
+// console.log(price);
+
